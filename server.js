7729f5812fc5f257d88d72824c6ce923cee50a4f
Aaron Jackson <aarontjackson@outlook.com>
2019-11-26 22:22:56 -0500

    index displays through localhost

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 
 // Routes
 require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 const syncOptions = { force: false };
 
