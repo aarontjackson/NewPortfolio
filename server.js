88e53f5c747469ec5d9dfae718772d8ad251a47a
Aaron Jackson <aarontjackson@outlook.com>
2019-11-26 22:11:13 -0500

    synced contact form with SQL database

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
-
+const express = require("express");
 const db = require("./models");
 
 const app = express();
 let PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // Routes
 require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
 const syncOptions = { force: false };
 
@@ -20,13 +20,13 @@ if (process.env.NODE_ENV === "test") {
     syncOptions.force = false;
 }
 
-db.sequelize.sync(syncOptions).then(function() {
-    app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+    app.listen(PORT, function () {
         console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+            PORT,
+            PORT
+        );
     });
 });
 
