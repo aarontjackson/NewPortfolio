774b7324cef0d91d7e389c582a9e955a08f3266e
Aaron Jackson <aarontjackson@outlook.com>
2019-12-13 12:48:17 -0500

    css animation to horizontal-links and control a elements in header section

--- public/js/index.js
+++ public/js/index.js
@@ -23,31 +23,74 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-  // contact form submission
-  $('#contact-submit').on('click', function (event) {
 
-    event.preventDefault();
+var scroll = window.requestAnimationFrame ||
+  // IE Fallback
+  function (callback) { window.setTimeout(callback, 1000 / 60) };
+var elementsToShow = document.querySelectorAll('.picture-image');
 
-    let newContact = {
-      name: $('#name').val().trim(),
-      email: $('#email').val().trim(),
-      comment: $('#comment').val().trim()
-    };
+function loop() {
 
-    console.log(newContact);
-
-    $.post("/api/contact", newContact)
-
-      .then(function () {
-        let row = $("<div>");
-        row.addClass("contact");
-        row.append("<p>" + newContact.name + "</p>");
-        row.append("<p>" + newContact.email + "</p>");
-        row.append("<p>" + newContact.comment + "</p>");
-      });
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
   });
+
+  scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+// contact form submission
+$('#contact-submit').on('click', function (event) {
+
+  event.preventDefault();
+
+  let newContact = {
+    name: $('#name').val().trim(),
+    email: $('#email').val().trim(),
+    comment: $('#comment').val().trim()
+  };
+
+  console.log(newContact);
+
+  $.post("/api/contact", newContact)
+
+    .then(function () {
+      let row = $("<div>");
+      row.addClass("contact");
+      row.append("<p>" + newContact.name + "</p>");
+      row.append("<p>" + newContact.email + "</p>");
+      row.append("<p>" + newContact.comment + "</p>");
+    });
 
   $('#log').text('Message sent!');
   $('#name').val("");
   $('#email').val("");
   $('#comment').val("");
+});
+
+
